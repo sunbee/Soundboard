64d9d4ee959082daf1ffb0c7a2fed34447da955d
sunbee <sanjay.bhatikar@gmail.com>
2023-05-24 13:35:25 -0500

    Full-featured, ready to deploy.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,5 +9,29 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
+  audioPlayer: HTMLAudioElement = new Audio();
+  nowPlaying: string = '';
+
   constructor() {}
+
+  playAudio(audioUrl: string) {
+    if (this.nowPlaying === audioUrl) { // This is what is playing already
+      this.audioPlayer.pause();
+      this.audioPlayer.currentTime = 0;
+      this.nowPlaying = '';
+    } else { // Stop and switch tracks
+      if (this.nowPlaying) { // Stop if playing
+        this.audioPlayer.pause();
+        this.audioPlayer.currentTime = 0;     
+      } 
+      // Switch tracks
+      this.audioPlayer.src = audioUrl;
+      this.audioPlayer.load();
+      this.audioPlayer.play();  
+      this.nowPlaying = audioUrl;
+    }
+  }
 }
+
+
+
